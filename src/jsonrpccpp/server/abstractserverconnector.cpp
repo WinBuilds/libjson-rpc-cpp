65415a44a5c3ab2b7443df421e38a2ae19e37218
latsa <laca@gmx.us>
2018-08-08 01:47:41 +0300

    updated abstractserverconnector

--- src/jsonrpccpp/server/abstractserverconnector.cpp
+++ src/jsonrpccpp/server/abstractserverconnector.cpp
@@ -18,6 +18,21 @@ AbstractServerConnector::AbstractServerConnector() { this->handler = NULL; }
 
 AbstractServerConnector::~AbstractServerConnector() {}
 
+bool AbstractServerConnector::OnRequest(const std::string& request, void* addInfo)
+{
+    string response;
+    if (this->handler != NULL)
+    {
+        this->handler->HandleRequest(request, response);
+        this->SendResponse(response, addInfo);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void AbstractServerConnector::ProcessRequest(const string &request,
                                              string &response) {
   if (this->handler != NULL) {
